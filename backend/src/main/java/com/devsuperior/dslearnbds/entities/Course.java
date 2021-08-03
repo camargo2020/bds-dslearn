d8bfda914d1c8af053429e27d0e2b1159ea69d70
camargo-devs <camargo.bruno.dev@gmail.com>
2021-08-03 15:00:52 -0300

    Offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,24 +1,30 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_course")
 public class Course implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
 
 	public Course() {
 
@@ -62,6 +68,10 @@ public class Course implements Serializable {
 
 	public void setImgGrayUri(String imagGrayUri) {
 		this.imgGrayUri = imagGrayUri;
+	}
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
